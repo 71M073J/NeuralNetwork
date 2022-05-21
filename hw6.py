d892d0ecb920d5bc03b7c60034a11291b09b7537
71M073J <tc7476@student.uni-lj.si>
2022-05-21 23:34:36 +0200

    yaaaay...
    kill me

--- hw6.py
+++ hw6.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 import numpy as np
 import scipy.optimize as op
@@ -15,13 +16,16 @@ def SigmoidBack(y, err):
 
 
 def cross_E(y_pred, y_true):  # CE
-    y_t = np.zeros((y_true.shape[0], y_true.shape[0]))
-    y_t[range(len(y_true)), y_true] = 1
-    return -np.sum(y_t * np.log(y_pred + 10 ** -100))
+    ce = -np.sum(y_true * np.log(y_pred + 10**-100))
+    return ce
+    #y_t = np.zeros((y_true.shape[0], y_true.shape[0]))
+    #y_t[range(len(y_true)), y_true] = 1
+    #return -np.sum(y_t * np.log(y_pred + 10 ** -100))
 
 
 def RMSE(ypred, ytrue):
-    return np.sqrt(np.power(ytrue - ypred.T, 2).mean())
+    return 0.5 * np.sum(np.power(ytrue - ypred.T, 2)) # why do i need this
+    #return np.sqrt(np.power(ytrue - ypred.T, 2).mean())
 
 
 def cross_E_grad(y_true, y_pred):  # CE derivative
@@ -31,10 +35,17 @@ def cross_E_grad(y_true, y_pred):  # CE derivative
 def ReLUBackward(y, err):
     return err * (y > 0)
 
+def ReLUPrime(y):
+    return (y > 0).astype(int)
 
 def ReLU(x):
     return np.maximum(0, x)
 
+
+def onehot(y):
+    y_new = np.zeros((y.shape[0], np.unique(y).shape[0]))
+    y_new[range(len(y)), y] = 1
+    return y_new.astype(int)
 
 def SoftMaxBack(y, err):
     # I = np.eye(y.shape[0])
@@ -67,19 +78,16 @@ def LinearBack(y, err):
 class NN:
     def __init__(self, n_hidden_layers, layer_sizes, input_size, output_size, classification=False,
                  batch_size=64, lr=0.01, bfgs=True, lambda_=0.0001):
-        #np.random.seed(0)
         self.lambda_ = lambda_
         self.lr = lr
         self.batch_size = batch_size
         self.bfgs = bfgs
         if classification:
             self.loss = cross_E
-            self.activation = SoftMax
-            self.activation_back = SoftMaxBack
         else:
             self.loss = RMSE
-            self.activation_back = ReLUBackward
-            self.activation = ReLU
+        self.activation_back = ReLUBackward
+        self.activation = ReLU
 
         self.layers = []
         if n_hidden_layers > 0:
@@ -119,13 +127,29 @@ class NN:
         # dzlast = err
         # dWlast = self.layers[-1].previous_activated.T.dot(dzlast)
         # dblast = dzlast
+        #err = prediction - y
         grads = []
-        for layer in reversed(self.layers):
-            activation_err = self.activation_back(layer.out, err)  # odvod po ????
-            grad = layer.previous_activated.T.dot(activation_err)  # /self.batch_size
-            grad = np.vstack((grad, err))
-            grads.append(grad)
-            err = layer.weights.dot(activation_err.T).T
+        gradz = np.atleast_2d(err).T
+        gradw = np.atleast_2d(err).T.dot(self.layers[-1].previous_activated).T
+        gradb = err
+        dlast = np.vstack((gradw, gradb))
+        grads.append(dlast)
+        for l, layer in enumerate(reversed(self.layers)):
+            if l == 0: continue
+            gradz = self.layers[-l].weights.dot(gradz) * ReLUPrime(layer.z).T
+            gradw = gradz.dot(layer.previous_activated) + layer.weights.T * self.lambda_ #???
+            gradb = gradz
+            dlast = np.vstack((gradw.T, gradb.T))
+            grads.append(dlast)
+
+
+        #TO NE DELA VREDU
+        #for layer in reversed(self.layers):
+        #    activation_err = self.activation_back(layer.out, err)  # odvod po ????
+        #    grad = layer.previous_activated.T.dot(activation_err)  # /self.batch_size
+        #    grad = np.vstack((grad, err))
+        #    grads.append(grad)
+        #    err = layer.weights.dot(activation_err.T).T
         grads.reverse()
         gradarr = np.zeros(sum([len(x.reshape(-1)) for x in grads]))
         cur = 0
@@ -136,28 +160,38 @@ class NN:
 
     def batch_grad(self, X, y):
         prediction = self.forward(np.atleast_2d(X[0, :]))
-        predictions = np.zeros((X.shape[0], prediction.shape[0]))
+        predictions = np.zeros((X.shape[0], prediction.shape[1]))
         predictions[0] = prediction
         # supposedly isto za multinomial softmax kot za MSE za regresijo
         gradvec = self.backward(prediction.flatten() - y[0])
         for i in range(1, X.shape[0]):
             prediction = self.forward(np.atleast_2d(X[i, :])).flatten()
             predictions[i] = prediction
-            gradvec += self.backward(prediction - y[i])
+            grad_input_i = self.backward(prediction - y[i])
+            gradvec += grad_input_i
         return gradvec / X.shape[0], predictions
 
     def weights_to_loss(self, w, *args):
         X, y = args[0], args[1]
         cur = 0
+        saved_weights = []
+        saved_biases = []
         for layer in self.layers:
+            saved_weights.append(layer.weights.copy())
+            saved_biases.append(layer.biases.copy())
             layer.weights[:, :] = w[cur:cur + layer.n_outputs * layer.n_inputs].reshape(
                 (layer.n_inputs, layer.n_outputs))
             cur += layer.n_inputs * layer.n_outputs
             layer.biases[:, :] = w[cur:cur + layer.n_outputs]
             cur += layer.n_outputs
+
         #prediction = self.forward(X)
         gradients, prediction = self.batch_grad(X, y)
         loss = self.loss(prediction, y)
+        for i, layer in enumerate(self.layers):
+            layer.weights = saved_weights[i].copy()
+            layer.biases = saved_biases[i].copy()
+
         return loss, gradients
 
     def update_weights(self, w):
@@ -166,7 +200,6 @@ class NN:
         for layer in self.layers:
             layer.weights[:, :] = w[cur:cur + layer.n_outputs * layer.n_inputs].reshape(
                 (layer.n_inputs, layer.n_outputs)).copy()
-            layer.weights += layer.weights * self.lambda_
             cur += layer.n_inputs * layer.n_outputs
             layer.biases[:, :] = w[cur:cur + layer.n_outputs].copy()
             cur += layer.n_outputs
@@ -183,14 +216,31 @@ class NN:
             layer.biases[:, :] -= g[cur:cur + layer.n_outputs] * lr
             cur += layer.n_outputs
 
-    def train(self, X, y, n_epochs, test=None, verbose=False):
-        scores = []
+    def check_grad(self, x0, X_train, y, i, to, yt):
+
+        #check gradient
+        r = self.weights_to_loss(x0, X_train, y[i * self.batch_size:to]) #returna loss, gradient
+        print(r[1])
+        test_grad = np.zeros(x0.shape)
+        for h in range(x0.shape[0]):
+            wi = x0.copy()
+            wi[h] += 1e-5
+            loss_direction_i = self.weights_to_loss(wi, X_train, yt)[0] / X_train.shape[0]
+            grad_dir_i = (loss_direction_i - r[0] / X_train.shape[0])/1e-5
+            test_grad[h] += grad_dir_i
+        print(test_grad)
+        print(abs(test_grad - r[1]))
+
+    def train(self, X, y, n_epochs, test=None, verbose=False, report_epochs=10):
+
+        again = False
         mins = 123415123
         minws = 0
         first = True
         for epoch in range(n_epochs):
-            if verbose and epoch % 100 == 0: print(f"epoch {epoch}")
             # else: print(epoch, end="")
+            scores = []
+            hs = []
             for i in range((len(y) // self.batch_size) + 1):
                 if i * self.batch_size == len(y): break
                 to = (i + 1) * self.batch_size if (i + 1) * self.batch_size < len(y) else len(y)
@@ -211,30 +261,29 @@ class NN:
                 # ZAKAJ TO NE DELA
                 # res = op.fmin_l_bfgs_b(func=self.weights_to_loss, x0=x0, args=(X_train, y[i * self.batch_size:to]),
                 #                       approx_grad=False)
-                if self.bfgs:
-                    #res = op.minimize(fun=self.weights_to_loss, args=(X_train, y[i * self.batch_size:to]), x0=x0, method="Nelder-Mead")
-                    res = op.fmin_l_bfgs_b(func=self.weights_to_loss, args=(X_train, y[i * self.batch_size:to]),
-                                           x0=x0, factr=10, pgtol=1e-9
-                                           )  # ,
-                    scores.append(res[1])
-                    #print(res[1])
-                    #print(self.weights_to_loss(x0, X_train, y[i * self.batch_size:to]))
-                    # fprime=self.batch_grad)
-                    h = self.weights_to_loss(res[0], X_train, y[i * self.batch_size:to])[0]
-                    if h < mins:
-                        mins = h
-                        self.update_weights(res[0])
-                    #print(self.weights_to_loss(self.updated_weights, X_train, y[i * self.batch_size:to])[0])
-                    #print(self.forward(X_train).T)
 
+                yt = y[i * self.batch_size:to]
+                res = op.fmin_l_bfgs_b(func=self.weights_to_loss, args=(X_train, yt),
+                                       x0=x0#, factr=1e-5, pgtol=1e-9
+                                       )  # ,
+                scores.append(res[1])
+
+                self.update_weights(res[0])
+                #print(res[1])
+                #print(self.weights_to_loss(x0, X_train, y[i * self.batch_size:to]))
+                # fprime=self.batch_grad)
+                h = self.weights_to_loss(res[0], X_train, y[i * self.batch_size:to])[0]
+
+                hs.append(h)
+            if np.mean(hs) + 0.1 < mins:
+                mins = np.mean(hs)
+            else:
+                if again:
+                    return
                 else:
-                    #self.lr *= 10
-                    self.gd(x0, X_train, y, i, to, self.lr)
-                    #print(self.weights_to_loss(self.updated_weights, X_train, y[i * self.batch_size:to])[0])
-                    #print(self.forward(X_train).T)
-                # print(res[1])
-                # quit()
-        #self.update_weights(minws)
+                    again = True
+
+            if verbose and epoch % report_epochs == 0: print(f"epoch {epoch}, minloss {mins/X_train.shape[0]}")
 
 class Layer:
     def __init__(self, in_dim, out_dim, activation_fun, activation_back, lr, batch):
@@ -248,13 +297,15 @@ class Layer:
         self.lr = lr
         self.previous_activated = None
         self.out = None
+        self.z = None
 
     def forward(self, input_data, last=False):
         # if last:
         #    self.previous_activated = input_data
         # else:
         self.previous_activated = input_data  # np.hstack((np.ones((input_data.shape[0], 1)), input_data))
-        self.out = self.activation(self.previous_activated.dot(self.weights) + self.biases)
+        self.z = self.previous_activated.dot(self.weights) + self.biases
+        self.out = self.activation(self.z)
         return self.out
 
 
@@ -264,9 +315,9 @@ class ANNRegression:
         self.lambda_ = lambda_
         self.model = None
 
-    def fit(self, X, y, n_epochs=100, lr=0.1, batch=64):
-        self.model = NN(len(self.layers), self.layers, X.shape[1], 1, lr=lr, batch_size=batch)
-        self.model.train(X, y, n_epochs)
+    def fit(self, X, y, n_epochs=50, batch=64, verbose=False, rep_epochs=10):
+        self.model = NN(len(self.layers), self.layers, X.shape[1], 1, batch_size=batch)
+        self.model.train(X, y, n_epochs, verbose=verbose, report_epochs=rep_epochs)
         return self
 
     def predict(self, X):
@@ -282,10 +333,11 @@ class ANNClassification:
         self.lambda_ = lambda_
         self.model = None
 
-    def fit(self, X, y, n_epochs=50, lr=0.1, batch=64):
-        self.model = NN(len(self.layers), self.layers, X.shape[1], np.bincount(y).shape[0], lr=lr, batch_size=batch,
+    def fit(self, X, y, n_epochs=50, batch=64, verbose=False, rep_epochs=10):
+        self.model = NN(len(self.layers), self.layers, X.shape[1], np.unique(y).shape[0], batch_size=batch,
                         classification=True)
-        self.model.train(X, y, n_epochs)
+        y = onehot(y)
+        self.model.train(X, y, n_epochs, verbose=verbose, report_epochs=rep_epochs)
         return self
 
     def predict(self, X):
@@ -294,45 +346,104 @@ class ANNClassification:
     def weights(self):
         return [np.vstack((layer.biases.reshape(1, -1), layer.weights)) for layer in self.model.layers]
 
+def load_csvdata(filename):
+    if filename == "housing3.csv":
+        y_class = True
+    else:
+        y_class = False
+    with open(filename) as f:
+        l = f.readline()
+        lines = []
+        ys = []
+        for line in f:
+            tokens = line.split(",")
+            lines.append([float(x) for x in tokens[:-1]])
+            if y_class:
+                ys.append(int(tokens[-1][1]) - 1)
+            else:
+                ys.append(float(tokens[-1]))
+    return np.array(lines), np.array(ys)
+def three():
+    from sklearn.model_selection import train_test_split
+    from sklearn import linear_model
+    from sklearn.ensemble import RandomForestClassifier
+    m1 = []
+    m2 = []
+    for i in range(20):
+        reg = linear_model.LinearRegression()
+        data, cls = load_csvdata("housing2r.csv")
+        #print("loaded regression data")
+        X_train, X_test, y_train, y_test = train_test_split(data, cls, test_size=0.33, random_state=420)
+        nn = ANNRegression([5], 0.01)
+        #print("split data, fitting")
+        nn.fit(X_train, y_train, verbose=False, rep_epochs=1, batch=999999999)
+        #print("fit nn")
+        reg.fit(X_train, y_train)
+        #print("fit both models")
+        p1 = nn.predict(X_test)
+        p2 = reg.predict(X_test)
+        m1.append(np.sqrt(np.mean(np.power(p1 - y_test, 2))))
+        m2.append(np.sqrt(np.mean(np.power(p2 - y_test, 2))))
+    print("mean, std:")
+    print("nn", np.mean(m1), np.std(m1))
+    print("linreg", np.mean(m2), np.std(m2))
+    m3 = []
+    m4 = []
+    for i in range(20):
+        reg = RandomForestClassifier(max_depth=5, n_estimators=100, max_features=5)
+        data, cls = load_csvdata("housing3.csv")
+        #print("loaded classification data")
+        X_train, X_test, y_train, y_test = train_test_split(data, cls, test_size=0.33, random_state=420)
+        nn = ANNClassification([5], 0.001)
+        #print("split data, fitting")
+        nn.fit(X_train, y_train.astype(int), verbose=False, rep_epochs=1, batch=999999999)
+        #print("fit nn")
+        reg.fit(X_train, y_train)
+        #print("fit both models")
+        p1 = nn.predict(X_test)
+        p2 = reg.predict(X_test)
+        m3.append(cross_E(p1, onehot(y_test)))
+        m4.append(cross_E(p2, y_test))
+    print("mean, std:")
+    print("nn", np.mean(m3), np.std(m3))
+    print("random forest\n", np.mean(m4), np.std(m4))
+
+def create_final_predictions():
+    def load_four_train(name):
+        with open(name) as f:
+            f.readline()
+            lines = []
+            ys = []
+            for line in f:
+                tokens = line.split(",")
+                lines.append([float(x) for x in tokens[1:-1]])
+                ys.append(int(tokens[-1][-2]) - 1)
+        return np.array(lines), np.array(ys)
+    def load_four_test(name):
+        with open(name) as f:
+            f.readline()
+            lines = []
+            for line in f:
+                tokens = line.split(",")
+                lines.append([float(x) for x in tokens[1:]])
+        return np.array(lines)
+    X_train, y_train = load_four_train("train.csv")
+    X_test = load_four_test("test.csv")
+    print(X_train.shape, y_train.shape)
+    nn = ANNClassification([64, 16], 0.1)
+    start = time.time()
+    nn.fit(X_train, y_train.astype(int), verbose=True, rep_epochs=1, batch=999999999)
+    end = time.time()
+    p = nn.predict(X_test)
+    endpred = time.time()
+    np.set_printoptions(threshold=np.inf)
+    with open("final.txt", "w") as f:
+        print(",".join(['id', 'Class_1', 'Class_2', 'Class_3', 'Class_4',
+                                         'Class_5', 'Class_6', 'Class_7', 'Class_8', 'Class_9']), file=f)
+        for i in range(p.shape[0]):
+            print(str(i) + "," + ",".join([str(x) for x in p[i]]), file=f)
+        print(f"total time elapsed: {end - start} seconds for fitting, \n{endpred - end} seconds for predicting test")
+
 if __name__ == "__main__":
-    data = np.random.randint(-5, 5, (5024, 2))
-    y = (data[:, 0] + data[:, 1]) > 1
-    y = data.sum(axis=1)
-    # data = np.array([[1,2],[2,3],[3,4],[4,5]])
-    # y = np.array((0,0,1,1))
-    # input = np.array([[1, 2]])
-    # target = np.array([[0, 1, 0]])
-    # norm = np.linalg.norm(input, axis=1)
-    # input_n = input / norm
-    # data = input_n
-    # y = target
-
-    # net = NN(1, 25, 2, 1, lr=0.01)
-    # net.train(data, y, 200)
-    # print(net.forward(np.array([[3, 0],[0,1], [-4,4], [4,1]])))
-    # print(net.forward(np.array([[2, 0],[1,0],[0,2], [0,4], [0,3],[4,0],[3,0]])))
-
-    # quit()
-    # np.random.seed(2)
-    net = NN(1, [10], 2, 1, lr=1)
-    X = np.array([[0, 0],
-                  [0, 1],
-                  [1, 0],
-                  [1, 1]])
-    y = np.array((0, 1, 2, 3))
-    hard_y = np.array((0, 1, 1, 0))
-
-    # TODO TODO TODO TODO TODO TODO TODO REGULARIZACIJAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    #fitter = ANNClassification(units=[], lambda_=0.0001)
-    #m = fitter.fit(X, y)
-    #pred = m.predict(X)
-    #np.testing.assert_allclose(pred, np.identity(4), atol=0.01)
-
-    fitter = ANNRegression(units=[], lambda_=0.0001)
-    m = fitter.fit(X, y, n_epochs=5, lr=0.1, batch=64)
-    pred = m.predict(X)
-    print(pred)
-    # np.testing.assert_allclose(pred, hard_y, atol=0.01)
-    # https: // www.adeveloperdiary.com / data - science / deep - learning / neural - network - with-softmax - in -python /
-    # https: // medium.com / @ neuralthreads / backpropagation - made - super - easy -
-    # for -you - part - 2 - 7b2a06f25f3c
+    #three()
+    create_final_predictions()
